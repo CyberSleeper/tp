478db17215e1500a9d8c17409b4c08b1bb12f85c
CyberSleeper <mahartha.gemilang@gmail.com>
2023-04-21 23:30:05 +0700

    finish NotaManager

--- assignment3/src/main/java/assignments/assignment3/nota/NotaManager.java
+++ assignment3/src/main/java/assignments/assignment3/nota/NotaManager.java
@@ -12,7 +12,9 @@ public class NotaManager {
      * Skips ke hari berikutnya dan update semua entri nota yang sesuai.
      */
     public static void toNextDay(){
-        //TODO: implement skip hari
+        for (Nota nota:notaList) {
+            nota.toNextDay();
+        }
     }
 
     /**
@@ -21,6 +23,11 @@ public class NotaManager {
      * @param nota Nota object untuk ditambahkan.
      */
     public static void addNota(Nota nota){
-        //TODO: implement add nota
+        Nota[] newNotaList = new Nota[notaList.length + 1];
+        for (int i = 0; i < notaList.length; i++) {
+            newNotaList[i] = notaList[i];
+        }
+        newNotaList[notaList.length] = nota;
+        notaList = newNotaList;
     }
 }
